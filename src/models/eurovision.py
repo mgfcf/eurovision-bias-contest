b620d60e4c14a3d373352d51bcb01c600f74fdcb
Max <m.giller@tu-braunschweig.de>
2022-05-17 16:14:55 +0200

    Added idea of rankings

--- src/models/eurovision.py
+++ src/models/eurovision.py
@@ -12,10 +12,22 @@ class Country:
     """Short name of country."""
 
 
+class Ranking:
+    pass
+
+
 @dataclass
 class Contest:
     """A eurovision song contest instance in one specific year."""
 
+    first_semi: Ranking
+    second_semi: Ranking
+
+    final: Ranking
+
 
 class Eurovision:
+    """Basic structure holding all information about the eurovision song contests."""
+
     contests: list[Contest]
+    """List of all ever contests."""
